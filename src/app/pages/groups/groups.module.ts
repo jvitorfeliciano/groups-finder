8195e9982c4ed52935aa13522cc5339ea5215d23
João Vitor Feliciano <jvitorfeliciano1@gmail.com>
2023-06-12 22:22:40 -0300

    feat: add groups page layout

--- src/app/pages/groups/groups.module.ts
+++ src/app/pages/groups/groups.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GroupsPageComponent } from './groups-page/groups-page.component';
+import { GroupComponent } from './group/group.component';
 
 @NgModule({
-  declarations: [GroupsPageComponent],
+  declarations: [GroupsPageComponent, GroupComponent],
   imports: [CommonModule],
   exports: [GroupsPageComponent],
 })
