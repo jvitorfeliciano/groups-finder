6b2586fbe9d28bd87dd84dd0622631318567a79a
João Vitor Feliciano <jvitorfeliciano1@gmail.com>
2023-06-12 21:40:15 -0300

    feat: add button component

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './home-page/home-page.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 
 @NgModule({
   declarations: [HomePageComponent],
-  imports: [CommonModule, SharedModule],
+  imports: [CommonModule, SharedModule, AppRoutingModule],
 
 })
 export class HomeModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from '../app-routing.module';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, ButtonComponent],
   imports: [CommonModule, AppRoutingModule],
-  exports: [HeaderComponent],
+  exports: [HeaderComponent, ButtonComponent],
 })
 export class SharedModule {}
